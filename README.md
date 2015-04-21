868e3d7245408c6ce984c661e1fe9ff45065d8eb
Josh Crawford <josh@engramdesign.com.au>
2015-04-21 19:27:51 +1000

    initial commit
    Signed-off-by: Josh Crawford <josh@engramdesign.com.au>

---
+++ README.md
@@ -0,0 +1,10 @@
+# Expanded Singles
+
+A simple plugin for Craft CMS that alters the Entries Index sidebar to list all Singles, rather than grouping them under a 'Singles' link.
+
+Functionality can be turned off without uninstalling the plugin, reverting to the original layout.
+
+Clicking on a Single in the menu will re-load the main index table with your lone Single. To go straight to editing the single, turn on the 'Redirect to Entry' option in the plugin settings.
+
+<img src="https://raw.githubusercontent.com/engram-design/ExpandedSingles/master/screenshots/main.png" width="200" />
+
---
+++ expandedsingles/ExpandedSinglesPlugin.php
@@ -0,0 +1,106 @@
+<?php
+namespace Craft;
+
+class ExpandedSinglesPlugin extends BasePlugin
+{
+    /* --------------------------------------------------------------
+    * PLUGIN INFO
+    * ------------------------------------------------------------ */
+
+    public function getName()
+    {
+        return Craft::t('Expanded Singles');
+    }
+
+    public function getVersion()
+    {
+        return '0.1';
+    }
+
+    public function getDeveloper()
+    {
+        return 'S. Group';
+    }
+
+    public function getDeveloperUrl()
+    {
+        return 'http://sgroup.com.au';
+    }
+
+    public function getSettingsHtml()
+    {
+        return craft()->templates->render( 'expandedsingles/settings', array(
+            'settings' => $this->getSettings(),
+        ) );
+    }
+
+    protected function defineSettings()
+    {
+        return array(
+            'expandSingles' => array( AttributeType::Bool, 'default' => true ),
+            'redirectToEntry' => array( AttributeType::Bool, 'default' => false ),
+        );
+    }
+
+    public function createSinglesList(&$sources)
+    {
+        $singles[] = array('heading' => 'Singles');
+
+        // Grab all the Singles
+        $singleSections = craft()->sections->getSectionsByType(SectionType::Single);
+
+        // Create list of Singles
+        foreach ($singleSections as $single) {
+            $criteria = craft()->elements->getCriteria(ElementType::Entry);
+            $criteria->sectionId = $single->id;
+            $entry = $criteria->first();
+
+            $url = $entry->getCpEditUrl();
+
+            $singles['single:'.$single->id] = array(
+                'label'     => $single->name,
+                'data'      => array('url' => $url),
+                'criteria'  => array('section' => $single),
+            );
+        }
+
+        // Insert it right after 'All Entries'
+        array_splice($sources, 1, 0, $singles);
+
+        // Remove original Singles lin
+        unset($sources['singles']);
+
+        // Insert some JS to go straight to single page when clicked - rather than listing in Index Table
+        if ($this->getSettings()->redirectToEntry) {
+            $js = '$(function() {' .
+                '$(document).on("click", ".content.has-sidebar #sidebar nav a[data-url]", function(e) {' .
+                    'e.preventDefault();' .
+                    '$(this).removeClass("sel");' .
+                    'location.href = $(this).attr("data-url")' .
+                '});' .
+            '});';
+
+            craft()->templates->includeJs($js);
+        }
+    }
+
+
+
+    /* --------------------------------------------------------------
+    * HOOKS
+    * ------------------------------------------------------------ */
+
+    public function modifyEntrySources(&$sources, $context)
+    {
+        if ($this->getSettings()->expandSingles) {
+
+            // Are there any Singles at all?
+            if (array_key_exists('singles', $sources)) {
+                if ($context == 'index') {
+                    $this->createSinglesList($sources);
+                }
+            }
+        }
+    }
+ 
+}
