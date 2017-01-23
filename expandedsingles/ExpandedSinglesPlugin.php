0409720cfef9df15b91d273830166a7efc845d3c
Josh Crawford <joshua@sgroup.com.au>
2017-01-24 09:58:30 +1100

    version 0.2.4

--- expandedsingles/ExpandedSinglesPlugin.php
+++ expandedsingles/ExpandedSinglesPlugin.php
@@ -14,7 +14,7 @@ class ExpandedSinglesPlugin extends BasePlugin
 
     public function getVersion()
     {
-        return '0.2.3';
+        return '0.2.4';
     }
 
     public function getSchemaVersion()
