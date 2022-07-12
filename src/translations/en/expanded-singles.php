39d60cfb359ef4ebddb76cf15fdeec28eb9499a6
Josh Crawford <joshua@sgroup.com.au>
2022-07-12 15:52:34 +1000

    Translations

---
+++ src/translations/en/expanded-singles.php
@@ -0,0 +1,11 @@
+<?php
+
+return array (
+	'Expanded Singles' => 'Expanded Singles',
+	'Settings' => 'Settings',
+	'Expand Singles' => 'Expand Singles',
+	'Expands the Singles link on the Entries page to list them like Channels and Structures.' => 'Expands the Singles link on the Entries page to list them like Channels and Structures.',
+	'Redirect to Entry' => 'Redirect to Entry',
+	'Automatically redirects to edit the Single Entry when clicking on the item in the sidebar.' => 'Automatically redirects to edit the Single Entry when clicking on the item in the sidebar.',
+	'Singles' => 'Singles',
+);
